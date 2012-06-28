deb7e0158630468d930bf6af94e59ec68e801f05
Clayton <clayton@claytonlz.com>
2012-06-27 17:52:31 -0700

    test_take_all, test_put_all

--- ruby/src/put_and_take.rb
+++ ruby/src/put_and_take.rb
@@ -1,11 +1,26 @@
 class PutAndTake
+
+  attr_accessor :tokens
+
+  def initialize(args = {})
+    self.tokens = args[:tokens]
+  end
+
   def evaluate(number, action)
-    tokens = number.to_i
+    tokens_to_take = take_pot_or_number?(number)
 
     if take_or_put?(action)
-      return tokens * -1
+      return tokens_to_take * -1
     else
+      return tokens_to_take
+    end
+  end
+
+  def take_pot_or_number?(number)
+    if number == "A"
       return tokens
+    else
+      return number.to_i
     end
   end
 
--- ruby/test/put_and_take_test.rb
+++ ruby/test/put_and_take_test.rb
@@ -3,6 +3,11 @@ require 'test_runner'
 class PutAndTakeTest < Test::Unit::TestCase
   def setup
     @game = PutAndTake.new
+    @ten_token_game = PutAndTake.new(:tokens => 10)
+  end
+
+  def test_game_should_have_a_pot
+    assert_equal(@ten_token_game.tokens, 10)
   end
 
   def test_put_one
@@ -21,5 +26,17 @@ class PutAndTakeTest < Test::Unit::TestCase
     result = @game.evaluate("1", "T")
 
     assert_equal(result, 1)
+  end
+
+  def test_take_all
+    result = @ten_token_game.evaluate("A", "T")
+
+    assert_equal(result, 10)
+  end
+
+  def test_put_all
+    result = @ten_token_game.evaluate("A", "P")
+
+    assert_equal(result, -10)
   end
 end
