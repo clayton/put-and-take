0747f4b6aa7ed8231cbd6eef977b87a16cc2da95
Clayton <clayton@claytonlz.com>
2012-06-27 17:40:15 -0700

    refactor

--- ruby/src/put_and_take.rb
+++ ruby/src/put_and_take.rb
@@ -2,10 +2,14 @@ class PutAndTake
   def evaluate(number, action)
     tokens = number.to_i
 
-    if action == "P"
+    if take_or_put?(action)
       return tokens * -1
     else
       return tokens
     end
+  end
+
+  def take_or_put?(action)
+    return true if action == "P"
   end
 end
